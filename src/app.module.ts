0e6e8d8b5bcd0ec5a94bc266137bfb8a3d471dc3
dvornick <mail@dvornick.ru>
2022-08-14 17:05:19 +0300

    Connect TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { dataSourceOptions } from './config/typeorm.config'
+import { UserModule } from './user/user.module'
 
-@Module({})
+@Module({
+	imports: [TypeOrmModule.forRoot(dataSourceOptions), UserModule],
+})
 export class AppModule {}
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,20 @@
+import { config } from 'dotenv'
+import { ConfigService } from '@nestjs/config'
+import { DataSource, DataSourceOptions } from 'typeorm'
+
+config({ path: '.typeorm.env' })
+const configService = new ConfigService()
+
+export const dataSourceOptions: DataSourceOptions = {
+	type: 'postgres',
+	username: configService.get('TYPEORM_USERNAME'),
+	password: configService.get('TYPEORM_PASSWORD'),
+	host: configService.get('TYPEORM_HOST'),
+	port: +configService.get('TYPEORM_PORT'),
+	database: configService.get('TYPEORM_DB'),
+	synchronize: configService.get('TYPEORM_SYNCHRONIZE') === 'true',
+	entities: ['src/**/*.entity.ts'],
+	migrations: ['src/migrations/*.ts'],
+}
+
+export const dataSource = new DataSource(dataSourceOptions)
