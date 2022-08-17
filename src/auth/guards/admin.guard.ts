62919f9f6e22895e7894c20433dcdb221104e527
dvornick <mail@dvornick.ru>
2022-08-17 15:24:43 +0300

    Remove extra console.log from AuthGuard

--- src/auth/guards/admin.guard.ts
+++ src/auth/guards/admin.guard.ts
@@ -10,7 +10,6 @@ import { UserEntity, UserRole } from 'src/user/user.entity'
 export class AdminGuard implements CanActivate {
 	canActivate(context: ExecutionContext): boolean {
 		const request = context.switchToHttp().getRequest<{ user: UserEntity }>()
-		console.log(request.user)
 		if (request.user.role !== UserRole.ADMIN)
 			throw new HttpException('Forbidden', 403)
 		else return true
